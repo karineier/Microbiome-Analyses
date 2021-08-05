d013bba7484ecc0b7d537e6897350676ed546090
Kari Neier <karineier@gmail.com>
2021-08-04 21:07:42 -0700

    Added in main effect of genotype and genotype by age interaction analyses

--- Longitudinal-Microbiome-Analysis.R
+++ Longitudinal-Microbiome-Analysis.R
@@ -114,5 +114,69 @@ tidyr::crossing(age = levels(as.factor(pdata$Age_weeks)),
     return(sigs)
   })
 
+###################################################################################################
+##### Refit model for estimation of genotype and genotype x age as main effect longitudinally #####
+###################################################################################################
 
+# separate male and female data since males have fewer time points #
+
+pdata$Age_weeks = as.factor(pdata$Age_weeks)
+
+longitudinalModel = function(sex) {
+  index = which(pdata$Sex == sex)
+  pdata.0 = pdata[index,]
+  pdata.0 = droplevels(pdata.0)
+  m.0 = m[,index]
+  
+  d.0 = DGEList(counts=m.0, genes=taxonomy, remove.zeros=T)
+  d.0 = calcNormFactors(d.0, method="RLE") # normalizing ASV data using RLE method
+  
+  if( !all(is.finite(d.0$samples$norm.factors))) {
+    stop("Something wrong with edgeR::calcNormFactors on this data,
+       non-finite $norm.factors, consider changing 'method' argument")
+  }
+  
+  mm.0 <- model.matrix( ~ Age_weeks*Genotype_cat + CF_Dam, data = pdata.0, contrasts=list(Age_weeks="contr.sum"))
+  y.0 = voom(d.0, mm.0, plot=T)
+  cor.0 = duplicateCorrelation(y.0, mm.0, block=pdata.0$Mouse_ID)$consensus
+  
+  fit.0 = lmFit(y.0, mm.0, correlation=cor.0, block=pdata.0$Mouse_ID)
+  head(coef(fit.0))
+  
+  ### F-test of interaction coefficients ###
+  
+  pdata.0$CF_Dam = as.factor(pdata.0$CF_Dam)
+  n.0 = 1 + nlevels(pdata.0$Age_weeks) + nlevels(pdata.0$CF_Dam)
+  n.1 = ncol(coef(fit.0))
+  tmp <- contrasts.fit(fit.0, coef=n.0:n.1)
+  tmp <- eBayes(tmp)
+  tmp2 <- topTable(tmp, sort.by="F", n=Inf)
+  tmp2$Taxa <- rownames(tmp2)
+  tmp2 <- tmp2[,c("Taxa", "AveExpr", "F", "P.Value", "adj.P.Val")]
+  cat("Number of taxa with significant time-genotype interaction in", ifelse(sex=="F", "Females", "Males"), "is", length(which(tmp2$adj.P.Val<0.05)), "\n")
+  
+  #sigs <- c(sigs, tmp2$Taxa[which(tmp2$adj.P.Val <0.05)])
+  sigtab = cbind(as(tmp2, "data.frame"), as(tax_table(ps)[rownames(tmp2), ], "matrix"))
+  filename <- glue::glue("Time_Genotype_Interaction_{sex}.txt")
+  write.table(file=filename, sigtab, col.names=T, row.names=F, sep="\t", quote=F)
+  
+  ### contrasts for genotype as main effect ###
+  
+  n.2 = nlevels(pdata.0$Age_weeks) + 1
+  tmp.0 <- contrasts.fit(fit.0, coef = n.2)
+  tmp.0 <- eBayes(tmp.0)
+  tmp2.0 <- topTable(tmp.0, sort.by="P", n=Inf)
+  tmp2.0$Taxa <- rownames(tmp2.0)
+  tmp2.0 <- tmp2.0[,c("Taxa", "AveExpr", "B", "P.Value", "adj.P.Val")]
+  cat("Number of taxa with significant main effect of genotype in", ifelse(sex=="F", "Females", "Males"), "is", length(which(tmp2.0$adj.P.Val<0.05)), "\n")
+  
+  sigtab.0 = cbind(as(tmp2.0, "data.frame"), as(tax_table(ps)[rownames(tmp2.0), ], "matrix"))
+  filename.0 <- glue::glue("Main_Effect_Genotype_{sex}.txt")
+  write.table(file=filename.0, sigtab.0, col.names=T, row.names=F, sep="\t", quote=F)
+  
+}
+
+sexes = c("F", "M")
+
+lapply(sexes, longitudinalModel)
 
